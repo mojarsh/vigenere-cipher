ef04fba0f90b1e5fcbeab9a9128f3322f9593521
Joe Marshall <joemarshall97@live.co.uk>
2024-07-26 22:46:55 +0100

    Added unit test for generation of encryption stream

--- tests/test_vigenere.py
+++ tests/test_vigenere.py
@@ -16,3 +16,7 @@ def test_vigenere_cipher_instance() -> None:
 def test_string_indexing() -> None:
     indexed_string = vigenere.index_string(base_string="DCBA", index=indexed_alphabet)
     assert indexed_string == ["3", "2", "1", "0"]
+
+
+def test_encryption_stream_generation() -> None:
+    assert vigenere.generate_encryption_stream("TESTSTRING") == "EXAMPLEEXA"
