d9e2bf22dafc25c4527430229bb9040f6ea93ad5
Joe Marshall <joemarshall97@live.co.uk>
2024-07-26 22:38:22 +0100

    Implemented unit tests using pytest

---
+++ tests/test_vigenere.py
@@ -0,0 +1,18 @@
+import string
+
+from src.vigenere import VigenereCipher
+
+CIPHER_KEY = "TEST"
+ENCRYPTION_KEY = "EXAMPLE"
+
+indexed_alphabet = {chr(i + 65): str(i) for i in range(len(string.ascii_uppercase))}
+vigenere = VigenereCipher(CIPHER_KEY, ENCRYPTION_KEY)
+
+
+def test_vigenere_cipher_instance() -> None:
+    assert isinstance(vigenere, VigenereCipher)
+
+
+def test_string_indexing() -> None:
+    indexed_string = vigenere.index_string(base_string="DCBA", index=indexed_alphabet)
+    assert indexed_string == ["3", "2", "1", "0"]
