5bff6dd3c9fb37263fd9ffb0af83de2821080778
Joe Marshall <joemarshall97@live.co.uk>
2024-07-27 19:58:19 +0100

    Removed Vigenere class to simplify encryption process

--- main.py
+++ main.py
@@ -1,49 +1,13 @@
-import string
+from src.vigenere import vigenere_encrypt
 
-import pandas as pd
-
-from src.vigenere import VigenereCipher
-
-CIPHER_KEY = "KRYPTOS"
-ENCRYPTION_KEY = "CIPHER"
+ENCRYPTION_KEY = "EXAMPLE"
 STRING_TO_ENCRYPT = "TESTEROFTHISCIPHER"
 
 
 def main() -> None:
     """Main function runs encryption process."""
 
-    vigenere = VigenereCipher(CIPHER_KEY, ENCRYPTION_KEY)
-    encryption_stream = vigenere.generate_encryption_stream(
-        string_to_encrypt=STRING_TO_ENCRYPT
-    )
-    indexed_alphabet = {chr(i + 65): str(i) for i in range(len(string.ascii_uppercase))}
-    indexed_encryption_string = vigenere.index_string(
-        STRING_TO_ENCRYPT, indexed_alphabet
-    )
-    indexed_encryption_stream = vigenere.index_string(
-        encryption_stream, indexed_alphabet
-    )
-
-    first_vigenere_row = list(CIPHER_KEY) + [
-        char for char in list(string.ascii_uppercase) if char not in list(CIPHER_KEY)
-    ]
-    vigenere_rows = []
-    for i in range(len(string.ascii_uppercase)):
-        last_letter = first_vigenere_row[-i:]
-        next_vigenere_row = first_vigenere_row[0:-i]
-        next_vigenere_row[:0] = last_letter
-        vigenere_rows.append(next_vigenere_row)
-
-    vigenere_table = pd.DataFrame(
-        vigenere_rows, columns=[_ for _ in range(len(string.ascii_uppercase))]
-    )
-    vigenere_table.to_csv(f"ref/{CIPHER_KEY}_VIGENERE_TABLE.csv")
-
-    encrypted_string = ""
-    for idx, col in zip(indexed_encryption_string, indexed_encryption_stream):
-        encrypted_string += vigenere_table[int(idx)].iloc[int(col)]
-
-    print(encrypted_string)
+    print(vigenere_encrypt(ENCRYPTION_KEY, STRING_TO_ENCRYPT))
 
 
 if __name__ == "__main__":
--- src/vigenere.py
+++ src/vigenere.py
@@ -1,13 +1,53 @@
-class VigenereCipher:
-    def __init__(self, cipher_key: str, encryption_key: str):
-        self.cipher_key = cipher_key
-        self.encryption_key = encryption_key
+import string
 
-    def index_string(self, base_string: str, index: dict) -> list:
-        """Replaces each letter in string with numerical index value."""
-        return [char.replace(char, index[char]) for char in base_string]
+from pandas import DataFrame
 
-    def generate_encryption_stream(self, string_to_encrypt: str) -> str:
-        """Uses computation of remainder to generate encryption stream for given encryption key."""
-        a, b = divmod(len(string_to_encrypt), len(self.encryption_key))
-        return self.encryption_key * a + self.encryption_key[:b]
+
+def index_string(base_string: str, index: dict) -> list:
+    """Replaces each letter in string with numerical index value."""
+    return [char.replace(char, index[char]) for char in base_string]
+
+
+def generate_indexed_alphabet() -> dict:
+    """Generates dict with letter keys matching to numerical values 0 - 25, to reference against Vigenere table."""
+    return {chr(i + 65): str(i) for i in range(len(string.ascii_uppercase))}
+
+
+def generate_encryption_stream(encryption_key: str, string_to_encrypt: str) -> str:
+    """Uses computation of remainder to generate encryption stream for given encryption key."""
+    a, b = divmod(len(string_to_encrypt), len(encryption_key))
+    return encryption_key * a + encryption_key[:b]
+
+
+def generate_vigenere_table() -> DataFrame:
+    """Creates Vigenere table used for message encryption and decryption."""
+
+    first_row = list(string.ascii_uppercase)
+    vigenere_rows = []
+    for i in range(len(string.ascii_uppercase)):
+        last_letter = first_row[-i:]
+        next_vigenere_row = first_row[0:-i]
+        next_vigenere_row[:0] = last_letter
+        vigenere_rows.append(next_vigenere_row)
+
+    return DataFrame(
+        vigenere_rows, columns=[num for num in range(len(string.ascii_uppercase))]
+    )
+
+
+def vigenere_encrypt(encryption_key: str, string_to_encrypt: str) -> str:
+    """Encrypts given string using Vigenere table."""
+    index = generate_indexed_alphabet()
+    encryption_stream = generate_encryption_stream(encryption_key, string_to_encrypt)
+    vigenere_table = generate_vigenere_table()
+    indexed_encryption_string = index_string(string_to_encrypt, index=index)
+    indexed_encryption_stream = index_string(encryption_stream, index=index)
+    encrypted_string = ""
+    for idx, col in zip(indexed_encryption_string, indexed_encryption_stream):
+        encrypted_string += vigenere_table[int(idx)].iloc[int(col)]
+
+    return encrypted_string
+
+
+def vigenere_decrypt() -> str:
+    pass
