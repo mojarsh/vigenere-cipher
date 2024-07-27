3b1ae980294e7672ec2d3493e5d4029546346abe
Joe Marshall <joemarshall97@live.co.uk>
2024-07-27 19:59:23 +0100

    Updated unit testing to cover encryption function

--- tests/test_vigenere.py
+++ tests/test_vigenere.py
@@ -1,22 +1,36 @@
-import string
+from src.vigenere import (
+    generate_encryption_stream,
+    generate_indexed_alphabet,
+    generate_vigenere_table,
+    index_string,
+    vigenere_encrypt,
+)
 
-from src.vigenere import VigenereCipher
-
-CIPHER_KEY = "TEST"
 ENCRYPTION_KEY = "EXAMPLE"
-
-indexed_alphabet = {chr(i + 65): str(i) for i in range(len(string.ascii_uppercase))}
-vigenere = VigenereCipher(CIPHER_KEY, ENCRYPTION_KEY)
-
-
-def test_vigenere_cipher_instance() -> None:
-    assert isinstance(vigenere, VigenereCipher)
+STRING_TO_ENCRYPT = "TESTEROFTHISCIPHER"
 
 
 def test_string_indexing() -> None:
-    indexed_string = vigenere.index_string(base_string="DCBA", index=indexed_alphabet)
+    indexed_alphabet = generate_indexed_alphabet()
+    indexed_string = index_string(base_string="DCBA", index=indexed_alphabet)
     assert indexed_string == ["3", "2", "1", "0"]
 
 
 def test_encryption_stream_generation() -> None:
-    assert vigenere.generate_encryption_stream("TESTSTRING") == "EXAMPLEEXA"
+    assert (
+        generate_encryption_stream(ENCRYPTION_KEY, STRING_TO_ENCRYPT)
+        == "EXAMPLEEXAMPLEEXAM"
+    )
+
+
+def test_generate_indexed_alphabet() -> None:
+    index = generate_indexed_alphabet()
+    assert index["A"] == "0"
+
+
+def test_vigenere_table() -> None:
+    assert generate_vigenere_table().shape == (26, 26)
+
+
+def test_encryption() -> None:
+    assert vigenere_encrypt(ENCRYPTION_KEY, STRING_TO_ENCRYPT) == "PHSHPGKBWHWDRELKEF"
